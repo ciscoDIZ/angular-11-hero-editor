ef8205f2d8056a329868244e35a22ab43dc8eb47
ciscodiz <ciscoadiz@gmail.com>
2021-02-11 14:07:27 +0000

    frustracion

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -17,7 +17,9 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 17, name: 'Jean Grey' },
       { id: 18, name: 'Rogue' },
       { id: 19, name: 'Elektra' },
-      { id: 20, name: 'Emma Frost' },
+      { id: 20, name: 'Emma Frost' }
+    ];
+    const villains = [
       {id: 21, name: 'Magneto'},
       {id: 22, name: 'Dr Doom'},
       {id: 23, name: 'Kingpin'},
@@ -29,8 +31,6 @@ export class InMemoryDataService implements InMemoryDbService {
       {id: 28, name: 'Dark Phoenix'},
       {id: 29, name: 'Sabretooth'},
       {id: 30, name: 'Mr. Sinister'}
-    ];
-    const villains = [
     ];
     return {heroes, villains};
   }
